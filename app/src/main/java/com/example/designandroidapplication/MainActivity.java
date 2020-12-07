72255f9c571b24af666cb08f5ef1e63e2feb3cc0
RazMunir <rm.c151010@gmail.com>
2020-12-07 17:39:47 +0600

    webview added + load a link

--- app/src/main/java/com/example/designandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/designandroidapplication/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String URL = "url";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void goToLoginUi(View view) {
         startActivity(new Intent(this, LoginUiActivity.class));
+    }
+
+    public void goToCode(View view) {
+
+        String url = "";
+        Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+
+        switch(view.getId()){
+            case R.id.login_ui:
+                url = "https://github.com/RazMunir/Design-Android/blob/main/app/src/main/res/layout/activity_login_ui.xml";
+                break;
+            default:
+                Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        intent.putExtra(URL, url);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/designandroidapplication/WebViewActivity.java
@@ -0,0 +1,42 @@
+package com.example.designandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    WebView webView;
+    String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        //retrieve data from intent extra
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null)
+        {
+            url = bundle.getString("url");
+        }
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new MyBrowser());
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(url);
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}

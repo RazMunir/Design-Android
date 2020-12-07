21bb1e610da9a72d8fd119fbefad97cf23f1525e
RazMunir <rm.c151010@gmail.com>
2020-12-07 18:04:23 +0600

    loginUi's all componenets added

--- app/src/main/java/com/example/designandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/designandroidapplication/MainActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebView;
-import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,18 +17,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //login UI
     public void goToLoginUi(View view) {
         startActivity(new Intent(this, LoginUiActivity.class));
     }
 
-    public void goToCode(View view) {
+    public void goToLoginUiComponents(View view) {
 
         String url = "";
         Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
 
-        switch(view.getId()){
-            case R.id.login_ui:
+        switch (view.getId()) {
+            case R.id.login_ui_code:
                 url = "https://github.com/RazMunir/Design-Android/blob/main/app/src/main/res/layout/activity_login_ui.xml";
+                break;
+            case R.id.login_ui_shape:
+                url = "https://github.com/RazMunir/Design-Android/blob/main/app/src/main/res/drawable/login_ui_buttonshapewhitebg.xml";
+                break;
+            case R.id.login_ui_color:
+                url = "https://github.com/RazMunir/Design-Android/blob/main/app/src/main/res/values/colors.xml";
+                break;
+            case R.id.login_ui_style:
+                url = "https://github.com/RazMunir/Design-Android/blob/main/app/src/main/res/values/themes.xml";
+                break;
+            case R.id.login_ui_font:
+                url = "https://github.com/RazMunir/Design-Android/blob/main/app/src/main/res/font/gotham.ttf";
                 break;
             default:
                 Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
